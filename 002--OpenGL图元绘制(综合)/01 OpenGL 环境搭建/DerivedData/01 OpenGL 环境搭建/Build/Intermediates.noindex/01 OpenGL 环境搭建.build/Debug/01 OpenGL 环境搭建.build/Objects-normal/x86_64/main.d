948543cdac7d48c978d388af60dae7e0c129d84a
yinxianxiaozi <2571118453@qq.com>
2021-03-07 20:59:10 +0800

    图元绘制的具体过程详细讲解

---
+++ 002--OpenGL图元绘制(综合)/01 OpenGL 环境搭建/DerivedData/01 OpenGL 环境搭建/Build/Intermediates.noindex/01 OpenGL 环境搭建.build/Debug/01 OpenGL 环境搭建.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,13 @@
+dependencies: \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/main.cpp \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLTools.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GL/glew.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/math3d.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLBatch.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLBatchBase.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLTriangleBatch.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLShaderManager.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLMatrixStack.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLFrame.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLFrustum.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源/02\ 胖C源码/001--OpenGL图元绘制(综合)/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLGeometryTransform.h
