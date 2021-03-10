6a4326e8f72c5133d2635bb730c2d6aed1892269
yinxianxiaozi <2571118453@qq.com>
2021-03-10 19:46:55 +0800

    渲染技巧

---
+++ 渲染技巧/001--正背面剔除和深度测试/01 OpenGL 环境搭建/DerivedData/01 OpenGL 环境搭建/Build/Intermediates.noindex/01 OpenGL 环境搭建.build/Debug/01 OpenGL 环境搭建.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,13 @@
+dependencies: \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/main.cpp \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLTools.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GL/glew.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/math3d.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLBatch.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLBatchBase.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLTriangleBatch.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLShaderManager.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLMatrixStack.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLFrame.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLFrustum.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/002--深度测试/01\ OpenGL\ 环境搭建/01\ OpenGL\ 环境搭建/include/GLGeometryTransform.h
---
+++ 渲染技巧/002--OpenGL 裁剪/DerivedData/01 OpenGL 环境搭建/Build/Intermediates.noindex/01 OpenGL 环境搭建.build/Debug/01 OpenGL 环境搭建.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/003--OpenGL\ 裁剪/01\ OpenGL\ 环境搭建/main.cpp \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/003--OpenGL\ 裁剪/01\ OpenGL\ 环境搭建/include/GLTools.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/003--OpenGL\ 裁剪/01\ OpenGL\ 环境搭建/include/GL/glew.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/003--OpenGL\ 裁剪/01\ OpenGL\ 环境搭建/include/math3d.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/003--OpenGL\ 裁剪/01\ OpenGL\ 环境搭建/include/GLBatch.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/003--OpenGL\ 裁剪/01\ OpenGL\ 环境搭建/include/GLBatchBase.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/003--OpenGL\ 裁剪/01\ OpenGL\ 环境搭建/include/GLTriangleBatch.h \
+  /Users/zhangwenyi/Downloads/05\ 学员资源\ 2/02\ 胖C源码/003--OpenGL\ 裁剪/01\ OpenGL\ 环境搭建/include/GLShaderManager.h
