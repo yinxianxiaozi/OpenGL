0645d091ec0f76e339b7ba1bb21b3d07ed1dad2a
yinxianxiaozi <2571118453@qq.com>
2021-04-26 08:08:21 +0800

    007-案例总结

---
+++ 007-总结案例/03 球体世界/DerivedData/01 OpenGL 环境搭建/Build/Intermediates.noindex/01 OpenGL 环境搭建.build/Debug/01 OpenGL 环境搭建.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,14 @@
+dependencies: \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/main.cpp \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/GLTools.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/GL/glew.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/math3d.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/GLBatch.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/GLBatchBase.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/GLTriangleBatch.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/GLShaderManager.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/GLFrustum.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/GLFrame.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/GLMatrixStack.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/GLGeometryTransform.h \
+  /Users/zhangwenyi/Desktop/z05631/未命名文件夹/资料/全训班课程资料/05\ 学员资源\ 6/01\ 源码/03\ 球体世界/01\ OpenGL\ 环境搭建/include/StopWatch.h
